35d0dbea2f9048983a6dbddb6d735565cd48f8b8
Alex Ganich <alexgar@bk.ru>
2020-04-23 02:30:09 +0300

    add folders

---
+++ Giftins/Data/ApplicationMarketContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Giftins.Core.Configuration;
+using Giftins.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Giftins.Web.Data
+{
+    public class ApplicationMarketContext : MarketDbContext
+    {
+        public ApplicationMarketContext(DbContextOptions<MarketDbContext> options , GiftinsConfig gconfig)
+            : base(options , gconfig)
+        {}
+    }
+}
