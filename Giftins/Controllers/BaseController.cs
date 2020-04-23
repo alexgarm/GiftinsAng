e796b5f486571a42ffa71014e01ed0c90b3a2cc5
Alex Ganich <alexgar@bk.ru>
2020-04-23 03:24:06 +0300

    Controllers

---
+++ Giftins/Controllers/BaseController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MediatR;
+
+namespace Giftins.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        /// <summary>
+        /// Redirect controller for external returnUrl
+        /// </summary>
+        protected virtual string REDIRECT_CONTROLLER => "Home";
+        /// <summary>
+        /// Redirect action for external returnUrl
+        /// </summary>
+        protected virtual string REDIRECT_ACTION => "Index";
+
+        protected readonly ILogger _logger;
+        protected readonly IMediator _mediator;
+
+        [TempData]
+        protected string StatusMessage { get; set; }
+
+        public BaseController(ILogger logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        protected IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+                return RedirectToLocal(returnUrl);
+
+            return RedirectToAction(REDIRECT_ACTION, REDIRECT_CONTROLLER, new { area = "" });
+        }
+    }
+}
---
+++ Giftins/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
+using MediatR;
+
+using Giftins.Core.Domain.User;
+
+namespace Giftins.Web.Controllers
+{
+    using Models;
+
+    public partial class HomeController : BaseController
+    {
+        private readonly SignInManager<GiftinsUser> _signInManager;
+
+        public HomeController(ILogger<HomeController> logger,
+            IMediator mediator,
+            SignInManager<GiftinsUser> signInManager)
+            : base(logger, mediator)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            if (_signInManager.IsSignedIn(User))
+                return RedirectToAction("Index", "User");
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Help()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
